f3bb6a3d780ff9b2a87c2914e5ae982278493b9b
Pranoti Mulay <pranoti.mulay@streetcontxt.com>
2024-03-23 23:50:07 -0400

    dynamic content generation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './assets/react-core-concepts.png';
 
+const reactCourseTypes = ["Fundamental", "Beginner", "Core", "Advanced"];
+
 function Header() {
   return (
     <header>
       <img src={logo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        { reactCourseTypes[Math.floor(Math.random()* 3)] } React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
