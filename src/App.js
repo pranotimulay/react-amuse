2c9746ac0cfb45755432ffe963602540a5b0321d
Pranoti Mulay <pranoti.mulay@streetcontxt.com>
2024-03-27 23:31:30 -0400

    props alternate way of display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import configImg from './assets/config.png';
+import jsxUi from './assets/jsx-ui.png';
+import stateMgmt from './assets/state-mgmt.png';
 
-const reactCourseTypes = ["Fundamental", "Beginner", "Core", "Advanced"];
+const reactCourseTypes = ["Fundamental", "Core", "Advanced"];
+
+const CORE_CONCEPTS = [
+    {
+        image: componentsImg,
+        title: 'Components',
+        description:
+            'The core UI building block - compose the user interface by combining multiple components.',
+    },
+    {
+        image: jsxUi,
+        title: 'JSX',
+        description:
+            'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+    },
+    {
+        image: configImg,
+        title: 'Props',
+        description:
+            'Make components configurable (and therefore reusable) by passing input data to them.',
+    },
+    {
+        image: stateMgmt,
+        title: 'State',
+        description:
+            'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+    },
+];
 
 function Header() {
     const courseTypeRandom = reactCourseTypes[Math.floor(Math.random() * 3)];
@@ -24,7 +54,7 @@ function CoreConcepts(data) {
 
     return (
       <li>
-          <img src={data.img} alt="Stylized Component"></img>
+          <img src={data.image} alt="Stylized Component"></img>
           <h3>{data.title}</h3>
           <p>{data.description}</p>
       </li>
@@ -40,8 +70,11 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcepts title="Components" description="The core UI building block" img={componentsImg}
-                                      testObject={{fistName: 'John', lastName: 'Smith'}}/>
+                        <CoreConcepts {...CORE_CONCEPTS[0]} />
+                        <CoreConcepts {...CORE_CONCEPTS[1]} />
+                        <CoreConcepts {...CORE_CONCEPTS[2]} />
+                        <CoreConcepts {...CORE_CONCEPTS[3]} />
+
                     </ul>
                 </section>
             </main>
