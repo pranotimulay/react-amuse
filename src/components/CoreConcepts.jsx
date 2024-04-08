8a5111c50e065274095ed2044d1b651dea3c12cb
Pranoti Mulay <55164245+pranotimulay@users.noreply.github.com>
2024-04-08 11:22:16 -0400

    commit to non-default branch

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -4,7 +4,7 @@ export default function CoreConcepts(data) {
 
     return (
         <li>
-            <img src={data.image} alt="Stylized Component"></img>
+            <img src={data.image} alt="Stylized Component Test"></img>
             <h3>{data.title}</h3>
             <p>{data.description}</p>
         </li>
