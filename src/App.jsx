68f78ec73c5a1a76b7fa811498ccfea127b8342a
Pranoti Mulay <55164245+pranotimulay@users.noreply.github.com>
2024-05-02 22:51:38 -0400

    key prop

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
                     <h2>Core Concepts</h2>
                     <ul>
                         {CORE_CONCEPTS.map((conceptItem) =>  
-                            (<CoreConcepts {...conceptItem} />)
+                            (<CoreConcepts key={conceptItem.title} {...conceptItem} />)
                         )};
 
                     </ul>
