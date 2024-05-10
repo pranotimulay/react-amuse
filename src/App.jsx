10fd3e6674b4838a98c193f392d54b771a6bddb2
Pranoti Mulay <55164245+pranotimulay@users.noreply.github.com>
2024-05-09 23:48:05 -0400

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
-import componentsImg from './assets/components.png';
-import configImg from './assets/config.png';
-import jsxUi from './assets/jsx-ui.png';
-import stateMgmt from './assets/state-mgmt.png';
 import Header from './components/Header.jsx';
 import {useState} from "react";
 import CoreConceptsSection from "./components/sections/CoreConceptsSection";
 import ExamplesSection from "./components/sections/ExamplesSection";
-
-
-const CORE_CONCEPTS = [
-    {
-        image: componentsImg,
-        title: 'Components',
-        description:
-            'The core UI building block - compose the user interface by combining multiple components.',
-    },
-    {
-        image: jsxUi,
-        title: 'JSX',
-        description:
-            'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
-    },
-    {
-        image: configImg,
-        title: 'Props',
-        description:
-            'Make components configurable (and therefore reusable) by passing input data to them.',
-    },
-    {
-        image: stateMgmt,
-        title: 'State',
-        description:
-            'React-managed data which, when changed, causes the component to re-render & the UI to update.',
-    },
-];
 
 
 function App() {
